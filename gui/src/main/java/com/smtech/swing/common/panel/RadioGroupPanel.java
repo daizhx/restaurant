3b7ece43fd4214be9f77c8fb61df1b6c84720bd1
Administrator <zhixindai@gmail.com>
2018-05-11 18:27:29 +0800

    aaa

---
+++ gui/src/main/java/com/smtech/swing/common/panel/RadioGroupPanel.java
@@ -0,0 +1,79 @@
+package com.smtech.swing.common.panel;
+
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+@SuppressWarnings("serial")
+public class RadioGroupPanel extends TransparentPanel {
+	
+	private int selectedIndex = -1;
+	
+	private OnSelectedListener listener;
+	
+	public interface OnSelectedListener{
+		void onSelected(int index);
+	}
+	
+	private MouseAdapter adapter = new MouseAdapter() {
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			Component c = e.getComponent();
+			if(c == null){
+				return;
+			}
+			SelectedAble sa;
+			if(c instanceof SelectedAble){
+				sa = (SelectedAble) c;
+			}else if(c.getParent() instanceof SelectedAble){
+				sa = (SelectedAble) c.getParent();
+			}else{
+				return;
+			}
+			SelectedAble preSel = (SelectedAble) getComponent(selectedIndex);
+			if(preSel == sa){
+				return;
+			}
+			preSel.setSelected(false);
+			sa.setSelected(true);
+			
+			selectedIndex = getComponentZOrder((Component)sa);
+			if(listener != null){
+				listener.onSelected(selectedIndex);
+			}
+		}
+	};
+
+	public RadioGroupPanel() {
+		super();
+	}
+
+	public RadioGroupPanel(LayoutManager l) {
+		super(l);
+	}
+
+	
+	@Override
+	protected void addImpl(Component comp, Object constraints, int index) {
+		super.addImpl(comp, constraints, index);
+		if(getComponentCount() == 1 && comp instanceof SelectedAble){
+			((SelectedAble)comp).setSelected(true);
+			selectedIndex = 0;
+		}
+		comp.addMouseListener(adapter);
+	}
+
+	public OnSelectedListener getOnSelectedListener() {
+		return listener;
+	}
+
+	public void setOnSelectedListener(OnSelectedListener listener) {
+		this.listener = listener;
+	}
+
+	//获取当前选中的的控件index
+	public int getSelectedIndex() {
+		return selectedIndex;
+	}
+	
+}
---
+++ gui/src/main/java/com/smtech/swing/common/panel/SelectedAble.java
@@ -0,0 +1,8 @@
+package com.smtech.swing.common.panel;
+
+//gui控制可被选选中
+public interface SelectedAble {
+	
+	void setSelected(boolean b);
+	
+}
