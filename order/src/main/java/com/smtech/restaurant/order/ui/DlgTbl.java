9eac65c4a5f6627adeff19224c0914b89114d0b0
Administrator <zhixindai@gmail.com>
2018-11-24 17:27:18 +0800

    aaa

--- order/src/main/java/com/smtech/restaurant/order/ui/DlgTbl.java
+++ order/src/main/java/com/smtech/restaurant/order/ui/DlgTbl.java
@@ -4,6 +4,9 @@ import com.smtech.swing.common.dlgs.DlgBase;
 import com.smtech.swing.common.view.ViewGroup;
 import org.springframework.stereotype.Component;
 
+/**
+ * 桌台界面
+ */
 @Component
 public class DlgTbl extends DlgBase{
 
